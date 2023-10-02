2cb0b88c791e757c36798f2dd9f16bac18c18ebf
rzyvrsln <rzayevruslan893@gmail.com>
2023-10-02 12:00:42 +0400

    committed

---
+++ E-commerceBookShop/BookShopDto/Dtos/AppUserDtos/AppUserLoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShopDto.Dtos.AppUserDtos
+{
+    public class AppUserLoginDto
+    {
+        public string UserName { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+    }
+}
--- E-commerceBookShop/BookShopViewModel/Entites/ConfirmMailVM.cs
+++ E-commerceBookShop/BookShopViewModel/Entites/ConfirmMailVM.cs
@@ -3,6 +3,6 @@
     public class ConfirmMailVM
     {
         public string Email { get; set; }
-        public int ConfirmCode { get; set; }
+        public string ConfirmCode { get; set; }
     }
 }
